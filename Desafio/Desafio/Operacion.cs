8d8c1c038d22dc4bb45e6aacefcb1faa687f242b
Alan Sanjurjo <sanjurjoalan77@gmail.com>
2021-09-16 19:24:36 -0300

    Desafio clase 9

---
+++ Desafio/Desafio/Operacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio
+{
+    class Operacion
+    {
+        public static void Suma(int numero1, int numero2)
+        {
+            int resultado = numero1 + numero2;
+            Console.WriteLine("La suma de los 2 numeros es: "+resultado);
+        }
+
+        public static void Resta(int numero1, int numero2)
+        {
+            int resultado = numero1 - numero2;
+            Console.WriteLine("La resta de los 2 numeros es: " + resultado);
+        }
+
+    }
+}
---
+++ Desafio/Desafio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Desafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numero1;
+            int numero2;
+
+            Console.WriteLine("Ingrese el numero 1: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 2: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Operacion.Suma(numero1, numero2);
+            Operacion.Resta(numero1, numero2);
+
+        }
+    }
+}
