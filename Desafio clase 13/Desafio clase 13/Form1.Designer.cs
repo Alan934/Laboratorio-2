c4b75000c3caf275a0effb1016719b02f0915cfd
Alan Sanjurjo <sanjurjoalan77@gmail.com>
2021-09-30 22:50:00 -0300

    Desafio clase 13, problema en la resolucion

---
+++ Desafio clase 13/Desafio clase 13/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace Desafio_clase_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonPiso = new System.Windows.Forms.RadioButton();
+            this.radioButtonLocal = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPiso = new System.Windows.Forms.TextBox();
+            this.txtLocal = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtAntiguedad = new System.Windows.Forms.TextBox();
+            this.txtSuperficie = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.labelPrecioFinal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Location = new System.Drawing.Point(-312, -46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 135);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tipo de Inmueble";
+            // 
+            // radioButtonPiso
+            // 
+            this.radioButtonPiso.AutoSize = true;
+            this.radioButtonPiso.Location = new System.Drawing.Point(30, 57);
+            this.radioButtonPiso.Name = "radioButtonPiso";
+            this.radioButtonPiso.Size = new System.Drawing.Size(56, 21);
+            this.radioButtonPiso.TabIndex = 1;
+            this.radioButtonPiso.TabStop = true;
+            this.radioButtonPiso.Text = "Piso";
+            this.radioButtonPiso.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonLocal
+            // 
+            this.radioButtonLocal.AutoSize = true;
+            this.radioButtonLocal.Location = new System.Drawing.Point(156, 57);
+            this.radioButtonLocal.Name = "radioButtonLocal";
+            this.radioButtonLocal.Size = new System.Drawing.Size(63, 21);
+            this.radioButtonLocal.TabIndex = 2;
+            this.radioButtonLocal.TabStop = true;
+            this.radioButtonLocal.Text = "Local";
+            this.radioButtonLocal.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Piso Nro*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nro de ventanas*";
+            // 
+            // txtPiso
+            // 
+            this.txtPiso.Location = new System.Drawing.Point(30, 119);
+            this.txtPiso.Name = "txtPiso";
+            this.txtPiso.Size = new System.Drawing.Size(44, 22);
+            this.txtPiso.TabIndex = 5;
+            // 
+            // txtLocal
+            // 
+            this.txtLocal.Location = new System.Drawing.Point(156, 119);
+            this.txtLocal.Name = "txtLocal";
+            this.txtLocal.Size = new System.Drawing.Size(44, 22);
+            this.txtLocal.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 185);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(223, 244);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(44, 22);
+            this.txtPrecio.TabIndex = 8;
+            // 
+            // txtAntiguedad
+            // 
+            this.txtAntiguedad.Location = new System.Drawing.Point(123, 244);
+            this.txtAntiguedad.Name = "txtAntiguedad";
+            this.txtAntiguedad.Size = new System.Drawing.Size(44, 22);
+            this.txtAntiguedad.TabIndex = 9;
+            // 
+            // txtSuperficie
+            // 
+            this.txtSuperficie.Location = new System.Drawing.Point(30, 244);
+            this.txtSuperficie.Name = "txtSuperficie";
+            this.txtSuperficie.Size = new System.Drawing.Size(44, 22);
+            this.txtSuperficie.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Dirección";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Superficie*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(120, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Antiguedad*";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(220, 224);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Precio Base*";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(30, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 57);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Calcular Precio Final";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.bottonCalcular);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(173, 244);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 17);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "(años)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(80, 244);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 17);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "(m2)";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.Control;
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label10.Location = new System.Drawing.Point(27, 368);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(137, 17);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "El Precio Final es:  $";
+            // 
+            // labelPrecioFinal
+            // 
+            this.labelPrecioFinal.AutoSize = true;
+            this.labelPrecioFinal.Location = new System.Drawing.Point(200, 368);
+            this.labelPrecioFinal.Name = "labelPrecioFinal";
+            this.labelPrecioFinal.Size = new System.Drawing.Size(0, 17);
+            this.labelPrecioFinal.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 447);
+            this.Controls.Add(this.labelPrecioFinal);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSuperficie);
+            this.Controls.Add(this.txtAntiguedad);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtLocal);
+            this.Controls.Add(this.txtPiso);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButtonLocal);
+            this.Controls.Add(this.radioButtonPiso);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Inmoviliaria";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonPiso;
+        private System.Windows.Forms.RadioButton radioButtonLocal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPiso;
+        private System.Windows.Forms.TextBox txtLocal;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtAntiguedad;
+        private System.Windows.Forms.TextBox txtSuperficie;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label labelPrecioFinal;
+    }
+}
+
---
+++ Desafio clase 13/Desafio clase 13/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio_clase_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bottonCalcular(object sender, EventArgs e)
+        {
+            Calcular calculos;
+
+            /*String numeroPiso = txtPiso.Text;
+            String numeroVentanas = txtLocal.Text;
+            String precio = txtPrecio.Text;
+            String superficie = txtSuperficie.Text;
+            String añosAntiguedad = txtAntiguedad.Text;*/
+
+            int numeroPisoi = Int32.Parse(txtPiso.Text);
+            int numeroVentanasi = Int32.Parse(txtLocal.Text);
+            double preciod = double.Parse(txtPrecio.Text);
+            double superficied = double.Parse(txtSuperficie.Text);
+            int añosAntiguedadi = Int32.Parse(txtAntiguedad.Text); 
+
+             calculos = new Calcular(numeroPisoi, numeroVentanasi, preciod, superficied, añosAntiguedadi);
+            //Calcular calculos = new Calcular();
+            //calculos.NumeroPiso = txtPiso.Text;
+
+            if (radioButtonLocal.Checked == true)
+            {
+                double mostrarPrecio = double.Parse(labelPrecioFinal.Text);
+                mostrarPrecio = calculos.CalculosFinalLocal();
+                //labelPrecioFinal.Text = calculos.CalculosFinalLocal();
+                //MessageBox.Show("Precio a pagar: $");
+            }
+            if (radioButtonPiso.Checked == true)
+            {
+                double mostrarPrecio2 = double.Parse(labelPrecioFinal.Text);
+                mostrarPrecio2 = calculos.CalculosPiso();
+            }
+        }
+    }
+}
